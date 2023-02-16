31286d5038ed68195349985e117a6ff1ccd9d6e1
Aleksey2710 <Aleksey_2710@mail.ru>
2023-02-16 23:33:05 +0300

    initial commit

---
+++ src/test/java/pro/sky/recipesapp_2_0/RecipesApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.recipesapp_2_0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
